af2b12dcc5c3016449be2c6716291f5df961a19f
leonardo lira <leon_lira_@hotmail.com>
2019-11-29 10:31:18 -0600

    sf

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://localhost/admyo_web/back/';
+$config['base_url'] = 'http://localhost/admyo_web/back/';
  //$config['base_url'] = 'http://localhost/admyo/web/admyoback/';
-$config['base_url'] = 'https://test.admyo.com/back/';
+//$config['base_url'] = 'https://test.admyo.com/back/';
 /*
 |--------------------------------------------------------------------------
 | Index File
