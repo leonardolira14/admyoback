1e3ed72a35020923b960893c9efba1e18743ebf3
unknown <leon_lira_@hotmail.com>
2020-03-06 15:32:50 -0600

    asfd

--- application/models/Model_Notificaciones.php
+++ application/models/Model_Notificaciones.php
@@ -3,7 +3,7 @@
 /**
  * 
  */
-class Model_Notificaciones extends CI_Model
+class Model_notificaciones extends CI_Model
 {
 	
 	function __construct()
@@ -12,12 +12,12 @@ class Model_Notificaciones extends CI_Model
 		$this->load->database();
 	}
 	public function getnumten($_ID_Empresa){
-		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("notificaciones")->join("empresa","empresa.IDEmpresa=Notificaciones.IDEmpresaN")->where("Notificaciones.IDEmpresa='$_ID_Empresa'")->get();
+		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("notificaciones")->join("empresa","empresa.IDEmpresa=notificaciones.IDEmpresaN")->where("notificaciones.IDEmpresa='$_ID_Empresa'")->get();
 		return $respuesta->num_rows();
 		
 	}
 	public function getten($_ID_Empresa){
-		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("notificaciones")->join("empresa","empresa.IDEmpresa=Notificaciones.IDEmpresaN")->where("Notificaciones.IDEmpresa='$_ID_Empresa'")->get();
+		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("notificaciones")->join("empresa","empresa.IDEmpresa=notificaciones.IDEmpresaN")->where("notificaciones.IDEmpresa='$_ID_Empresa'")->get();
 		$notificaciones=[];
 		foreach ($respuesta->result_array() as $notificacion) {
 			array_push($notificaciones,array("Descript"=>$notificacion["Descript"],"IDUsuarioE"=>$notificacion["IDUsuarioE"],"IDNotificacion"=>$notificacion["IDNotificacion"],"IDEmpresaN"=>$notificacion["IDEmpresaN"],"Razon_Social"=>$notificacion["Razon_Social"],"Fecha"=>$notificacion["fecha"]));
