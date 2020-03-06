3d7aefe335ca9f53122ba9ad68a8f25aa34afb2c
unknown <leon_lira_@hotmail.com>
2020-03-06 15:28:46 -0600

    asfd

--- application/models/Model_Notificaciones.php
+++ application/models/Model_Notificaciones.php
@@ -12,12 +12,12 @@ class Model_Notificaciones extends CI_Model
 		$this->load->database();
 	}
 	public function getnumten($_ID_Empresa){
-		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("Notificaciones")->join("empresa","empresa.IDEmpresa=Notificaciones.IDEmpresaN")->where("Notificaciones.IDEmpresa='$_ID_Empresa'")->get();
+		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("notificaciones")->join("empresa","empresa.IDEmpresa=Notificaciones.IDEmpresaN")->where("Notificaciones.IDEmpresa='$_ID_Empresa'")->get();
 		return $respuesta->num_rows();
 		
 	}
 	public function getten($_ID_Empresa){
-		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("Notificaciones")->join("empresa","empresa.IDEmpresa=Notificaciones.IDEmpresaN")->where("Notificaciones.IDEmpresa='$_ID_Empresa'")->get();
+		$respuesta=$this->db->select("IDNotificacion,IDEmpresaN,Razon_Social,fecha,Descript,IDUsuarioE,")->From("notificaciones")->join("empresa","empresa.IDEmpresa=Notificaciones.IDEmpresaN")->where("Notificaciones.IDEmpresa='$_ID_Empresa'")->get();
 		$notificaciones=[];
 		foreach ($respuesta->result_array() as $notificacion) {
 			array_push($notificaciones,array("Descript"=>$notificacion["Descript"],"IDUsuarioE"=>$notificacion["IDUsuarioE"],"IDNotificacion"=>$notificacion["IDNotificacion"],"IDEmpresaN"=>$notificacion["IDEmpresaN"],"Razon_Social"=>$notificacion["Razon_Social"],"Fecha"=>$notificacion["fecha"]));
@@ -25,7 +25,7 @@ class Model_Notificaciones extends CI_Model
 		return $notificaciones;
 	}
 	public function delete($id){
-		$this->db->where("IDNotificacion='$id'")->delete('Notificaciones');
+		$this->db->where("IDNotificacion='$id'")->delete('notificaciones');
 	}
 	//funcion para agregar una notificacion
 	public function add($IDEmpresaN,$Descripcion,$IDEmpresa,$IDUsuarioE,$tipo){
@@ -45,7 +45,7 @@ class Model_Notificaciones extends CI_Model
 			"IDEmpresaN"=>$IDEmpresaN,
 			"IDUsuarioE"=>$IDUsuarioE
 		);
-		$this->db->insert("Notificaciones",$array);
+		$this->db->insert("notificaciones",$array);
 	}
 	public function getconfig($IDEmpresa){
 		$respuesta=$this->db->select('Configaletas')->where("IDEmpresa='$IDEmpresa'")->get('empresa');
