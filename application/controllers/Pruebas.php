c493b37e7356fabfe8a4bcbe5cf73f9717e8cf9d
unknown <leon_lira_@hotmail.com>
2021-05-19 13:39:27 -0500

    cambio de configuracion de base datos nada importante

--- application/controllers/Pruebas.php
+++ application/controllers/Pruebas.php
@@ -21,7 +21,7 @@ class Pruebas extends CI_Controller
 	public function prueva_correo_activar_usuario(){
 		$categorias = ['basic', 'micro', 'micro_anual', 'empresa', 'empresa_anual'];
 		$correos = ['bernardo@admyo.com','lira053@gmail.com','belen.roldan@admyo.com'];
-		$this->Model_Email->Activar_Usuario_registro('Token para activar', $correos, 'Leonardo', 'Lira ', $categorias[0], 'usuarioenadmyo', 'passwprd');
+		//$this->Model_Email->Activar_Usuario_registro('Token para activar', $correos, 'Leonardo', 'Lira ', $categorias[0], 'usuarioenadmyo', 'passwprd');
 		$this->Model_Email->Activar_Usuario_registro('Token para activar', $correos, 'Leonardo', 'Lira ', $categorias[1], 'usuarioenadmyo', 'passwprd');
 		$this->Model_Email->Activar_Usuario_registro('Token para activar', $correos, 'Leonardo', 'Lira ', $categorias[2], 'usuarioenadmyo', 'passwprd');
 		$this->Model_Email->Activar_Usuario_registro('Token para activar', $correos, 'Leonardo', 'Lira ', $categorias[3], 'usuarioenadmyo', 'passwprd');
